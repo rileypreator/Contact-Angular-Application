16b9e4c22e09a236d66d30a293fe4759ada9d03d
rileypreator <pre15001@byui.edu>
2020-10-10 15:45:05 -0600

    Added document implementation

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  //list of all documents
+  documents: Document[] = [
+    new Document("1", "Riley's Document", "My first document", "https://docs.google.com/document/d/1QH7JjKWXuEOJTHXDZIQjDZcQYzg6OLOVF4OeX1CdiO0/edit?usp=sharing", null),
+    new Document("1", "Riley Preator", "Test Document", "https://docs.google.com/document/d/1QH7JjKWXuEOJTHXDZIQjDZcQYzg6OLOVF4OeX1CdiO0/edit?usp=sharing", null)
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentsComponent implements OnInit {
 
+  selectedDocument = Document;
   constructor() { }
 
   ngOnInit(): void {
